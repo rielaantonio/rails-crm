5e6b94e49e5b6806d0af6547d6645a48e61c78d9
Andrew Dunkman <adunkman@gmail.com>
2008-06-30 16:42:02 -0500

    Added icons and updated person model to use gravitar

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,11 +3,19 @@ class Person < ActiveRecord::Base
     "#{first} #{last}"
   end
 
-  def authentication
-    AuthAccount.find (:first, :conditions => "person_id = '#{id}'");
+  def email_md5
+    Digest::MD5.hexdigest("adunkman@gmail.com")
   end
 
-  def has_authentication
-    true if authentication
+  def avatar_url
+    "http://www.gravatar.com/avatar/#{email_md5}?s=128"
+  end
+
+  def avatar_thumb_url
+    "http://www.gravatar.com/avatar/#{email_md5}?s=50"
+  end
+
+  def authentication
+    AuthAccount.find (:first, :conditions => "person_id = '#{id}'");
   end
 end
